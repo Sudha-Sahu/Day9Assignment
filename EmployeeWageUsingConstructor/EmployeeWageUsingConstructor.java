40e569c584b740d461febc87fe0f114fe4d26133
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-07 08:37:48 +0530

    Added-program to calculate empWage

--- EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
+++ EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
@@ -1,21 +1,29 @@
 package com.Bridgelabz.Day9Assignment;
 
 public class EmployeeWageUsingConstructor{
+	
+	public final int PARTTIME = 1;
+	public final int FULLTIME = 2;
+	public final int EMPRATEPERHR=20;
 
 	EmployeeWageUsingConstructor() {
-
+		
 		System.out.println("Welcome to Employee Wage Problem using Constructor!!!!");
 	}
 	EmployeeWageUsingConstructor(int isFullTime) {
-		
-		double empCheck = Math.floor (Math.random() * 10) % 2;
+	
+		int empHrs = 0;
+		int empWage = 0;
+		double empCheck = Math.floor (Math.random() * 10) % 2;                   
 		if (empCheck == isFullTime)
-			System.out.println("Employee is Present");
-		else        
-			System.out.println("Employee is Absent");
+			empHrs=8;
+		else            
+			empHrs=0;
+		empWage= empHrs * EMPRATEPERHR;
+		System.out.println("calculating EmpWage: "+empWage);
 	}
 	public static void main(String[] args) {
-
+		
 		EmployeeWageUsingConstructor emp = new EmployeeWageUsingConstructor();
 		emp = new EmployeeWageUsingConstructor(1);
 	}
