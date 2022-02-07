6dfe788f068368640dbd6b4b33bfc32767944c35
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-07 08:23:10 +0530

    Added-welcome to employee wage program using constructor

---
+++ EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.Day9Assignment;
+
+public class EmployeeWageUsingConstructor{
+
+	public EmployeeWageUsingConstructor() {
+
+		System.out.println("Welcome to Employee Wage Problem using Constructor!!!!");
+	}
+
+	public static void main(String[] args) {
+
+		EmployeeWageUsingConstructor emp = new EmployeeWageUsingConstructor();
+
+	}
+}
