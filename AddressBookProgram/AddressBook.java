5348eac4a929aeff700ab6a3b109090c4b6c69d3
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-07 21:03:16 +0530

    Added-welcome to Address Book

---
+++ AddressBookProgram/AddressBook.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.Day9Assignment;
+
+public class AddressBook{
+
+	AddressBook() {
+
+		System.out.println("Welcome to Address Book program in AddressBook Main class!!!!");
+	}
+	public static void main(String[] args) {
+
+		AddressBook address = new AddressBook();
+	}
+}
