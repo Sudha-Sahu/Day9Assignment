4285b5260e09bd7d09f0553664e5d4855e521099
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-07 09:28:01 +0530

    Added-calculating emp Wage for multiple companies

--- EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
+++ EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
@@ -4,19 +4,16 @@ public class EmployeeWageUsingConstructor{
 
 	public final int PART_TIME = 1;
 	public final int FULL_TIME = 2;
-	public final int EMP_RATE_PER_HR=20;
-	public final int MAX_NO_Of_WORKING_DAYS = 20;
-	public final int MAX_WORKING_HRS=100;
-
+	
 	EmployeeWageUsingConstructor() {
 
 		System.out.println("Welcome to Employee Wage Problem using Constructor!!!!");
 	}
-	public int calcEmployeeWage(int empHrs, int empWage, int empWageforMonth) {
+	public int calcEmployeeWage(String company, int empRatePerHr, int maxNoOfDays, int maxHrforMonth) {
 
-		int noOfDays = 0;
-		int noOfHrs = 0;
-		while(noOfDays < MAX_NO_Of_WORKING_DAYS && noOfHrs < MAX_WORKING_HRS) {
+		int noOfDays = 0, noOfHrs = 0;
+		int empHrs, empWage=0, empWageforMonth=0;
+		while(noOfDays < maxNoOfDays && noOfHrs < maxHrforMonth) {
 			int empCheck = (int) Math.floor (Math.random() * 10) % 3;
 			if (empCheck == PART_TIME)
 				empHrs=4;
@@ -26,16 +23,18 @@ public class EmployeeWageUsingConstructor{
 				empHrs=0;
 			noOfDays++;
 			noOfHrs++;
-			empWage= empHrs * EMP_RATE_PER_HR;
-			System.out.println("EmpWage in day : "+noOfDays+" is : "+empWage);
+			empWage= empHrs * empRatePerHr;
+			System.out.println(company+" EmpWage in day : "+noOfDays+" is : "+empWage);
 			empWageforMonth = empWageforMonth+empWage;
 		}
-		System.out.println("calculating EmpWage for month: "+ empWageforMonth);
-		return 0;
+		System.out.println("Total EmpWage for "+company+" in month is : "+ empWageforMonth);
+		return empWageforMonth;
 	}
 	public static void main(String[] args) {
 
 		EmployeeWageUsingConstructor emp = new EmployeeWageUsingConstructor();
-		emp.calcEmployeeWage(0, 0, 0);
+		emp.calcEmployeeWage("DMART", 20, 20, 100);
+		emp.calcEmployeeWage("Reliance", 10, 25, 150);
+		emp.calcEmployeeWage("Airtel", 15, 20, 140);
 	}
 }
