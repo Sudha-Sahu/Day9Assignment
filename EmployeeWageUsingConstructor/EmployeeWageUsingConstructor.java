7ebac154b7b4a04b2ebbf17286ec156417f4b4b9
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-07 09:01:49 +0530

    Added-Calculating employee wage for month

--- EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
+++ EmployeeWageUsingConstructor/EmployeeWageUsingConstructor.java
@@ -1,35 +1,42 @@
 package com.Bridgelabz.Day9Assignment;
 
 public class EmployeeWageUsingConstructor{
-	
+
 	public final int PARTTIME = 1;
 	public final int FULLTIME = 2;
 	public final int EMPRATEPERHR=20;
+	public final int NO_Of_WORKING_DAYS = 20;	
 
 	EmployeeWageUsingConstructor() {
-		
+
 		System.out.println("Welcome to Employee Wage Problem using Constructor!!!!");
 	}
-	public int calcEmployeeWage(int empHrs, int empWage) {
-	
-		int empCheck = (int) Math.floor (Math.random() * 10) % 3;
-		switch (empCheck) {
-		case 1:
-			empHrs=4;
-			break;
-		case 2:
-			empHrs=8;
-			break;
-		default:
-			empHrs=0;
+	public int calcEmployeeWage(int empHrs, int empWage, int empWageforMonth) {
+
+		int day = 0;
+		while(day < NO_Of_WORKING_DAYS) {
+			int empCheck = (int) Math.floor (Math.random() * 10) % 3;
+			switch (empCheck) {
+			case 1:
+				empHrs=4;
+				break;
+			case 2:
+				empHrs=8;
+				break;
+			default:
+				empHrs=0;
+			}
+			day++;
+			empWage= empHrs * EMPRATEPERHR;
+			System.out.println("EmpWage in day : "+day+" is : "+empWage);
+			empWageforMonth = empWageforMonth+empWage;
 		}
-		empWage= empHrs * EMPRATEPERHR;
-		System.out.println("calculating EmpWage: "+empWage);
+		System.out.println("calculating EmpWage for month: "+ empWageforMonth);
 		return 0;
 	}
 	public static void main(String[] args) {
-		
+
 		EmployeeWageUsingConstructor emp = new EmployeeWageUsingConstructor();
-		emp.calcEmployeeWage(0, 0);
+		emp.calcEmployeeWage(0, 0, 0);
 	}
 }
